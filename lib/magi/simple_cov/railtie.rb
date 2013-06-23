2d3ad8bf5892179891714de6c9603984ae81ed73
Ryo Nakamura <r7kamura@gmail.com>
2013-06-23 20:23:28 +0900

    Add autoload paths for development convenience

--- lib/magi/simple_cov/railtie.rb
+++ lib/magi/simple_cov/railtie.rb
@@ -3,6 +3,10 @@ require "rails/railtie"
 module Magi
   module SimpleCov
     class Railtie < Rails::Railtie
+      initializer "magi.simple_cov.set_autoload_paths", before: :set_autoload_paths do |app|
+        app.config.autoload_paths << File.expand_path("../../../", __FILE__)
+      end
+
       config.after_initialize do
         require "magi/simple_cov/initializer"
       end
